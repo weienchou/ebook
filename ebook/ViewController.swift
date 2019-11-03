10ebd0c2c952d029aeab89a2536581577f12d8c0
wayne <git@weien.tw>
2019-11-04 02:21:17 +0800

    first commit

---
+++ ebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ebook
+//
+//  Created by WAYNE on 2019/11/2.
+//  Copyright © 2019 WAYNE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
